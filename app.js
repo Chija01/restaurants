3303b96dd09861b316a6b7d7bc07aa7255bb9eae
Chija01 <janechiu@global.t-bird.edu>
2024-10-24 16:31:59 +0800

    use express-handebars as view engine

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const app = express()
+const { engine } = require('express-handlebars')
+
 const port = 3000
 
+app.engine('.hbs',engine({extname:'.hbs'}))
+app.set('view engine','.hbs')
+app.set('views','./views')
 app.use(express.static('public'))
+
 
 app.get('/', (req,res) =>{
   res.redirect('/restaurants')
 })
 
 app.get('/restaurants', (req, res) =>{
-  res.send('listing restaurants')
-
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res) =>{
