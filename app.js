7941e1d2b739491655b85bb093d8281de54dee74
Chija01 <janechiu@global.t-bird.edu>
2024-10-25 15:00:18 +0800

    add listing page

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express()
 const { engine } = require('express-handlebars')
 
 const port = 3000
+const restaurants = require('./public/jsons/restaurants.json').results
 
-app.engine('.hbs',engine({extname:'.hbs'}))
-app.set('view engine','.hbs')
-app.set('views','./views')
+app.engine('.hbs', engine({extname: '.hbs'})) //定義要使用的樣板引擎，其中參數 .hbs 是這個樣板引擎的名稱。
+app.set('view engine', '.hbs') //設定的 view engine 是 hbs (handlebars)。
+app.set('views', './views')
 app.use(express.static('public'))
 
 
@@ -14,12 +15,13 @@ app.get('/', (req,res) =>{
   res.redirect('/restaurants')
 })
 
-app.get('/restaurants', (req, res) =>{
-  res.render('index')
+app.get('/restaurants', (req, res) => {
+  res.render('index', { restaurants: restaurants })
 })
 
+
 app.get('/restaurant/:id', (req, res) =>{
-  const id = req.param.id
+  const id = req.params.id
   res.send(`read restaurant:${id}`)
 })
 
